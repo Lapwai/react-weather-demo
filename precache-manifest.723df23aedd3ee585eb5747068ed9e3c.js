2f12be2b52e11330461d72c3c2fa911fcc654c1f
unknown <lapwailau@gmail.com>
2020-01-29 19:14:26 +1300

    Updates

---
+++ precache-manifest.723df23aedd3ee585eb5747068ed9e3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777aeedb012b69257588afe3d81685b4",
+    "url": "/react-weather-demo/index.html"
+  },
+  {
+    "revision": "0713a4244bf4efa5f42c",
+    "url": "/react-weather-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "094d6584fd47c0bdb660",
+    "url": "/react-weather-demo/static/css/main.5fa31caf.chunk.css"
+  },
+  {
+    "revision": "0713a4244bf4efa5f42c",
+    "url": "/react-weather-demo/static/js/2.fead4ca9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-weather-demo/static/js/2.fead4ca9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "094d6584fd47c0bdb660",
+    "url": "/react-weather-demo/static/js/main.c6c4f749.chunk.js"
+  },
+  {
+    "revision": "c83059e169b088084c6b",
+    "url": "/react-weather-demo/static/js/runtime-main.b9606d81.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-demo/static/media/bg.b6796c73.jpg"
+  }
+]);
